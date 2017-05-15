bfdf3b49a0c396d28736b582b674be26c54415f0
Mike James <m@mikejam.es>
2017-05-15 12:47:11 +1000

    fix redux warning on user

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import configureStore, { Provider } from './lib/opinionated-redux';
 import config from './state';
 import App from './App';
 
-const initialState = { name: 'test' };
+const initialState = { user: { name: 'test' } };
 const store = configureStore(config, initialState);
 
 ReactDOM.render(
